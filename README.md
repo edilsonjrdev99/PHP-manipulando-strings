2ced836fababab1185ffb9fecf70799cfcce3150
Edilson Junior <edilsonjrdev99@gmail.com>
2025-09-25 20:32:15 -0300

    feat: Add replace examples for module 4

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 ### Conceitos do curso
 
-- Novidades do PHP 8
+- Novidades do PHP 8 e funções interessantes
 1. str_contains($stringComparada, 'valor Da String');
 2. str_starts_with($stringComparada, 'Valor da string');
 3. str_ends_with($stringComparada, 'Valor da String');
 4. strpos($stringComparada, 'string ou caractere');
+5. addslashes($string);
+6. htmlentities($string);
 
 - Como funciona a comparação de bytes do strlen e mb_strlen
+- Exemplos de separação e junção de strings
+- Aparando pontas de string
+- Entendendo melhor como funciona string numérica no PHP 8+
+- Delimitadores para string grandes inline
+- Funções que barram injection no HMTL
--- modulo-4.php
+++ modulo-4.php
@@ -34,3 +34,25 @@ sendEmailHeredoc('João de Barro');
 
 echo 'nowdoc é como se fosse áspas simples' . PHP_EOL;
 sendEmailNowdoc('João de barro');
+
+// Substituição de string
+echo '--- Substituição de string ---' . PHP_EOL;
+
+$message = 'Estou muito bravo com você maldito, foda em!';
+
+$badWords = ['miserável', 'foda', 'maldito'];
+$replaceWords = ['m***', 'f***', 'm***'];
+
+echo 'Exemplo de str_replace: ' . str_replace($badWords, $replaceWords, $message) . PHP_EOL;
+echo 'Exemplo de strtr: ' . strtr($message, 'foda', '***@') . PHP_EOL;
+echo 'Exemplo de strtr com array no parâmetro: ' . strtr($message, ['foda' => 'f***']) . PHP_EOL;
+
+// Comportamento de substituições igual
+echo '--- Comportamento de substituições iguais ---' . PHP_EOL;
+
+$text = 'hi all, I said hello';
+$translate = ['hello' => 'hi', 'hi' => 'hello'];
+
+echo "Texto - $text" . PHP_EOL;
+echo 'substituições iguais com strtr: ' . strtr($text, $translate) . PHP_EOL;
+echo 'substituições iguais com str_replace: ' . str_replace(['hello', 'hi'], ['hi', 'hello'], $text) . PHP_EOL;
